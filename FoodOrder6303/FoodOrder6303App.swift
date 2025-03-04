1d52e934c33507475adc7616d4dd5fc08ed12a3b
Alexander Pechinkin <pechinkin04@yandex.ru>
2025-03-04 13:39:39 +0400

    Initial Commit

---
+++ FoodOrder6303/FoodOrder6303App.swift
@@ -0,0 +1,17 @@
+//
+//  FoodOrder6303App.swift
+//  FoodOrder6303
+//
+//  Created by Александр Печинкин on 04.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodOrder6303App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
